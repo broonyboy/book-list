894a7651d95291ba3b86ffaf77cdae48c3af4263
broonyboy <nbrown3@sky.com>
2018-12-28 13:55:01 +0000

    add alert messaging

--- app.js
+++ app.js
@@ -50,6 +50,19 @@ class UI {
         }
     }
 
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+
+        // Vanish after 3 seconds
+
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
+
     static clearFields() {
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
@@ -76,18 +89,23 @@ document.querySelector('#book-form'), addEventListener('submit', (e) => {
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
-    //instantiate book
+    // Validate
 
-    const book = new Book(title, author, isbn);
+    if (title === '' || author === '' || isbn === '') {
+        UI.showAlert('Please fill in all fields', 'danger');
+    } else {
+        //instantiate book
 
-    // Add book to UI
+        const book = new Book(title, author, isbn);
 
-    UI.addBookToList(book);
+        // Add book to UI
 
-    // Clear fields
+        UI.addBookToList(book);
 
-    UI.clearFields();
+        // Clear fields
 
+        UI.clearFields();
+    }
 });
 //Event: remove a book
 
